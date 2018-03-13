6c6c49b7432e5224c10a9e1c49442927b97dd048
Magdalena Kier <magdalena.k1906@gmail.com>
2018-03-13 23:17:01 +0100

    ruletka poprawiona

--- src/pl/sdacademy/ruletka.java
+++ src/pl/sdacademy/ruletka.java
@@ -4,9 +4,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ruletka {
-    static Scanner scanner = new Scanner(System.in);
-    static int field;
-    static int cash = 1000;
+    private static Scanner scanner = new Scanner(System.in);
+    private static int field;
+    private static int cash = 1000;
 
     public static void main(String[] args) {
         rouletteGame();
@@ -16,37 +16,37 @@ public class ruletka {
     }
 
 
-    public static boolean rouletteGame() {
+    private static boolean rouletteGame() {
 
-        do {
-            int drawn = drawnNumber();
-            if (drawn % 2 == 0 && field == 2) {
-                System.out.println("Wygrałeś zakład, wylosowana liczba to: " + drawn);
-                return true;
-            } else if (drawn % 2 == 1 && field == 1) {
-                System.out.println("Wygrałeś zakład, wylosowana liczba to: " + drawn);
-                return true;
-            } else if (drawn % 2 == 0 && field == 1 || drawn %2 == 1 && field == 2) {
-                System.out.println("Przegrałeś zakład, wylosowana liczba to: " + drawn);
-                return false;
-            } else {
-                System.out.println();
-                return false;
-            }
-        } while ( betAmount() > 0);
+        {
+            do {
+                int drawn = drawnNumber();
+                if (drawn % 2 == 0 && field == 2) {
+                    System.out.println("Wygrałeś zakład, wylosowana liczba to: " + drawn);
+                    return true;
+                } else if (drawn % 2 == 1 && field == 1) {
+                    System.out.println("Wygrałeś zakład, wylosowana liczba to: " + drawn);
+                    return true;
+                } else if (drawn % 2 == 0 && field == 1 || drawn % 2 == 1 && field == 2) {
+                    System.out.println("Przegrałeś zakład, wylosowana liczba to: " + drawn);
+                    return false;
+                } else {
+                    System.out.println();
+                    return false;
+                }
+            } while (betAmount() > 0);
 
-
+        }
 
     }
 
-    public static int drawnNumber() {
+    private static int drawnNumber() {
         Random random = new Random();
-        int number = random.nextInt(36);
-        return number;
+        return random.nextInt(36);
 
     }
 
-    public static int betAmount() {
+    private static int betAmount() {
 
         while (cash > 0) {
             System.out.println("Podaj kwotę jaką chcesz obstawić, masz do dyspozycji: " + cash + " zł");
@@ -54,14 +54,14 @@ public class ruletka {
             System.out.println("Jakie pole chcesz obstawić:  czarne nieparzyste: 1, czerwone parzyste: 2");
             field = scanner.nextInt();
 
-            if (rouletteGame() == true) {
+            if (rouletteGame()) {
                 cash = cash + (amount * 2);
             } else {
                 cash = cash - (amount * 2);
             }
 
         }
-        System.out.println("Koniec gry");
+        System.out.println("Koniec gry - nie masz środków do dalszej gry");
         return cash;
     }
 }
